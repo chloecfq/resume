33218e63d49629f89de3cfb8a389cba723b9e833
OrangesChen <1345341069@qq.com>
2022-09-05 06:08:29 +0000

    Deploying to public from @ chloecfq/resume@247a5bb8fe49dcd2896ba0b4f5e59f4344e0e611 🚀

--- _next/static/n6_uw2a4IovX-QC1xLELK/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-eb138f99501ce6df22f7.js"],"/_error":["static/chunks/pages/_error-b2d29653cd468b43f3e4.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
