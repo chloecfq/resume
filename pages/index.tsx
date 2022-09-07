37e0c2862d3ab2811d6a501e1f4fc83b16952b9c
fengqun <chloe.chen@hispeedfox.com>
2022-09-07 17:13:08 +0800

    add mobile style

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,25 @@
+import { useEffect, useState } from 'react';
 import HHomePage from './templete/hbase';
+import VHomePage from './templete/vbase';
 
 
 const HomePage = (props) => {
+
+    const [isMobile, setIsMoblie] = useState(false)
+    useEffect(() => {
+        if (/Mobi|Android|iPhone/i.test(navigator.userAgent)) {
+            // 当前设备是移动设备
+            setIsMoblie(true)
+        }
+    }, [])
+
     return (
         // horizontal ? <HHomePage></HHomePage> :
         // <div>
         //     <div className='hidden print:block'><VHomePage></VHomePage></div>
         //     <div className='block print:hidden'><HHomePage></HHomePage></div>
         // </div>
-        <HHomePage></HHomePage>
+        isMobile ? <VHomePage></VHomePage> : <HHomePage></HHomePage>
         // <VHomePage></VHomePage>
     )
 }
